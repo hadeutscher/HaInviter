e4188d501bd4c87eca7ccd5253535f698f8a0597
Yuval Deutscher <yuval@deut.sh>
2026-09-18 10:39:37 +0300

    Revert "fix: fix some leftover issues"
    This reverts commit 178e7fb903fb0aea6c1322592fc1dce110ba27c5.

--- src/api.rs
+++ src/api.rs
@@ -10,10 +10,7 @@
 
 use crate::{
     i18n::Locale,
-    types::{
-        EventAdminView, EventInput, EventSummary, ImportSummary, InviteView, RsvpSubmission,
-        SeatPlacement,
-    },
+    types::{EventAdminView, EventInput, EventSummary, ImportSummary, InviteView, RsvpSubmission, SeatPlacement},
 };
 use dioxus::prelude::*;
 
@@ -223,7 +220,7 @@ pub async fn import_contacts(
     }
     if bytes.len() > MAX_VCF_BYTES {
         return Err(ServerFnError::new(
-            s().field_image_too_large
+            s().field_cover_too_large
                 .replace("{}", &(MAX_VCF_BYTES / (1024 * 1024)).to_string()),
         ));
     }
